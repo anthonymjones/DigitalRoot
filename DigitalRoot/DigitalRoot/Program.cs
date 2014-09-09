57e3f1d46bdf6b2def272f124a8c795927ff7086
anthonymjones <anthony.mjones@gmail.com>
2014-09-08 22:38:30 -0600

    Just Starting

---
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+    }
+        static void DigitalRoot(string rootThis)
+        {
+            
+            
+
+            
+          
+        }
+        static void rootingFunc(string root)
+        {
+            var total = 0;
+            for (var i = 0; i < root.Length; i++)
+            {
+                var rootNumber = root[i].ToString();
+                var number = Convert.ToInt32(rootNumber);
+                total = total + number;
+            }
+            Console.WriteLine(total);
+
+            Console.ReadKey();
+        }
+    
+}
